4fb07338f8488938c6a6cffda9b83a88cd8af201
manish -- <manish--@manishs-MacBook-Pro-2.local>
2025-02-28 18:52:20 +0000

    hd

--- shared/schema.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export const messageSchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    email: z.string().email(),
-    message: z.string(),
-    createdAt: z.date()
-});
-export const insertMessageSchema = messageSchema.omit({ id: true, createdAt: true });
